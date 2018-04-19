f730140e03a7c317152ad7132bb58201671dd62a
Andrew Blowe <ablowe@barquin.com>
2018-04-19 19:30:30 -0400

    switched to webpack 4

--- demo/demo.js
+++ demo/demo.js
@@ -1,8 +1,9 @@
-import World from '../src/ChoroplethTypes/World'
-import d3 from 'd3'
-import data from './demoData.json'
+// import World from '../src/ChoroplethTypes/World'
+// import d3 from 'd3'
+// import data from './demoData.json'
 
-d3.select('#demo-chart').text('hello')
-World('#demo-chart')
-  .data(data)
-  .draw()
+// d3.select('#demo-chart').text('hello')
+// World('#demo-chart')
+//   .data(data)
+//   .draw()
+console.log('hi')
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+
+module.exports = {
+  entry: {
+    'Choropleth-bundle': './src/Choropleth.js',
+    'demo-bundle': './demo/demo.js'
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/dist',
+    filename: '[name].js'
+  },
+  devServer: {
+    contentBase: './demo'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader' // create styles from JS strings
+          },
+          {
+            loader: 'css-loader' // translate CSS into CommonJS
+          },
+          {
+            loader: 'sass-loader' // compiles Sass to CSS
+          }
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js']
+  }
+}
